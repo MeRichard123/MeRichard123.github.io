7a5b46ac05a7072b90ced44526b048e3d02ea9ed
Richard <51269862+MeRichard123@users.noreply.github.com>
2021-08-26 13:12:35 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,7 @@
 #!/usr/bin/env sh
-
-# abort on errors
-set -e
-git checkout production
-# build
+mv docs build
 npm run build
-
-# navigate into the build output directory
-cd dist
-
-# add .nojekyll to bypass GitHub Page's default behavior
-touch .nojekyll
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git add -A
+mv build docs
+git add .
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -u origin production
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+git push -u origin main
