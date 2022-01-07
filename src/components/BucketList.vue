4d6cbe2353537c1ab3dd29493eaffdf8cee5c35e
Richard <rcoric03@gmail.com>
2022-01-07 17:39:04 +0000

    added goals and failed to sort by date

--- src/components/BucketList.vue
+++ src/components/BucketList.vue
@@ -29,34 +29,48 @@
 </template>
 
 <script>
-import {reactive} from "vue";
+import {reactive, computed} from "vue";
 
 export default{
   setup(){
       const data = reactive({
         goals: [
-            {title: "Get comfortable with React JS", completedDate: "Completed: 30/6/2020"},
-            {title: "Advance to more difficult React Topics", completedDate: "Completed: 30/6/2020"},
-            {title: "Learn backend development with Django", completedDate: "Completed: 15/9/2020"},
+            {title: "Get comfortable with React JS", completedDate: "Completed: 30/06/2020"},
+            {title: "Advance to more difficult React Topics", completedDate: "Completed: 30/06/2020"},
+            {title: "Learn backend development with Django", completedDate: "Completed: 15/09/2020"},
             {title: "Use Typescript with React", completedDate: "Completed: 3/2/2021"},
-            {title: "Use Django Rest Framework with React", completedDate: "Completed: 10/1/2021"},
-            {title: "Learn Vue3", completedDate: "Completed: 17/3/2021"},
-            {title: "Learn Express JS for APIs"},
-            {title: "Use VueRouter", completedDate: "Completed: 31/7/2021"},
-            {title: "Play with Websockets - Socket.io"},
-            {title: "Try VueX and Maybe look into ReduxToolkit", completedDate: "Completed: 8/8/2021"},
-            {title: "Learn Astro (Astro.build) - redo this site", completedDate: "Completed 26/8/2021"},
+            {title: "Use Django Rest Framework with React", completedDate: "Completed: 10/01/2021"},
+            {title: "Learn Vue3", completedDate: "Completed: 17/03/2021"},
+            {title: "Use VueRouter", completedDate: "Completed: 31/07/2021"},
+            {title: "Try VueX and Maybe look into ReduxToolkit", completedDate: "Completed: 8/08/2021"},
+            {title: "Learn Astro (Astro.build) - redo this site", completedDate: "Completed 26/08/2021"},
+            {title: "Be Comfortable with the basics of Express JS for APIs", completedDate: "Completed: 07/01/2022"},
             {title: "Learn GraphQL / Try GraphQl with express"},
             {title: "Try Graphene - Django GraphQl"},
-            {title: "Try FeathersJS"},
+            {title: "Play with Websockets - Socket.io"},
+            {title: "Try FeathersJS for Realtime Express"},
+            {title: "Play with NestJS for backend JS?"},
         ], 
         todos: [
             {title: "Continue learning German", completedDate: "Completed 26/8/2021"},
+            {title: "Get into The University of Lincoln", completedDate: "Completed: 16/12/2022"},
             {title: "Do exercise more often"},
             {title: "Get through A-Level exams with minimal stress"},
-            {title: "Get into The University of Lincoln", completedDate: "Completed: 16/12/2022"},
         ]
       })
+
+      // const SortedGoals = computed(() => {
+      //     data.goals.forEach((goal)=> {
+      //       console.log(goal.completedDate)
+      //       let sliced = goal.completedDate?.split(":")[1].trim();
+      //       let SplitDate = sliced?.split("/");
+      //       [SplitDate[0], SplitDate[1]] = [SplitDate[1], SplitDate[0]];
+            // goal.completedDate = new Date(SplitDate.join("/"));
+      //     })
+      //     return data.goals.sort((a,b) => {
+      //       return a.completedDate - b.completedDate;
+      //     }) 
+      // });
     
     const toggleGoalStatus = (index) => {
       let today = new Date();
@@ -76,7 +90,7 @@ export default{
     return {
       data,
       toggleGoalStatus,
-      toggleTodoStatus
+      toggleTodoStatus,
     }
   }
 }
